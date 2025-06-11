4aa177ce1552fb5b6844729e1925d4a5fa3d5589
pardipYadav <213sunilyadav@gmail.com>
2025-06-12 04:54:23 +0530

    null and undefined type

---
+++ Chapters_null_undefined/index.js
@@ -0,0 +1,9 @@
+var item = null;
+var item2 = "hello world";
+console.log(item);
+console.log(item2);
+var username = undefined;
+var val = null;
+// username = "hello worldsssss";
+console.log(username);
+console.log(val === username);
---
+++ Chapters_null_undefined/index.ts
@@ -0,0 +1,10 @@
+var item: null | string = null;
+var item2: null | string = "hello world";
+console.log(item);
+console.log(item2);
+
+var username: undefined | string = undefined;
+var val = null;
+// username = "hello worldsssss";
+console.log(username);
+console.log(val === username);
