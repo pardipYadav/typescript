55406d0246f435e9c2582bb066c153003305f420
pardipYadav <213sunilyadav@gmail.com>
2025-06-13 03:59:44 +0530

    bigInt type

---
+++ Output/Chapters_bigInt/index.d.ts
@@ -0,0 +1,5 @@
+declare var largeNumber: bigint;
+declare var x: bigint;
+declare var y: bigint;
+declare var total_num: bigint;
+declare var total_num_y: bigint;
---
+++ Output/Chapters_bigInt/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var largeNumber = 12345678901234567890n;
+var x = 1n;
+var y = 2n;
+var total_num = largeNumber + x;
+var total_num_y = largeNumber + y;
+console.log(total_num);
+console.log(total_num_y);
